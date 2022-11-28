e9df239413813c537bf94c01bff90ec52733af38
Saurabh <saurabhjaybhaye01@gmail.com>
2022-11-28 15:35:40 +0530

    working on json api

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SideBar.css";
 import Button from "./Button";
 
 const SideBar = () => {
+  const [stdClass, setStdClass] = useState("");
+  const [subInputs, setSubInputs] = useState([
+    {
+      subName: "",
+      subClassName: "",
+    },
+  ]);
+
+  const handleSubChange = (e) => {
+    const newvalues = subInputs.map((i) => {
+      i[e.target.name] = i[e.target.value];
+      return i;
+    });
+    setSubInputs(newvalues);
+  };
+
   const handleClassSubmit = (e) => {
     e.preventDefault();
+    console.log(stdClass);
   };
 
   const handleSubjectSubmit = (e) => {
     e.preventDefault();
+    console.log(subInputs);
   };
 
   const handleNotesSubmit = (e) => {
     e.preventDefault();
   };
+
   return (
     <div className="container mt-5">
-      <div class="accordion" id="accordionExample">
-        <div class="accordion-item">
-          <h2 class="accordion-header" id="headingOne">
+      <div className="accordion" id="accordionExample">
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingOne">
             <button
-              class="accordion-button"
+              className="accordion-button"
               type="button"
               data-bs-toggle="collapse"
               data-bs-target="#collapseOne"
@@ -32,22 +51,26 @@ const SideBar = () => {
           </h2>
           <div
             id="collapseOne"
-            class="accordion-collapse collapse show"
+            className="accordion-collapse collapse show"
             aria-labelledby="headingOne"
             data-bs-parent="#accordionExample"
           >
-            <div class="accordion-body">
+            <div className="accordion-body">
               <form onSubmit={handleClassSubmit}>
-                <input type="text" name="classname" />
+                <input
+                  type="text"
+                  name="classname"
+                  onChange={(e) => setStdClass(e.target.value)}
+                />
                 <Button type="submit">Add Class</Button>
               </form>
             </div>
           </div>
         </div>
-        <div class="accordion-item">
-          <h2 class="accordion-header" id="headingTwo">
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingTwo">
             <button
-              class="accordion-button collapsed"
+              className="accordion-button collapsed"
               type="button"
               data-bs-toggle="collapse"
               data-bs-target="#collapseTwo"
@@ -59,23 +82,33 @@ const SideBar = () => {
           </h2>
           <div
             id="collapseTwo"
-            class="accordion-collapse collapse"
+            className="accordion-collapse collapse"
             aria-labelledby="headingTwo"
             data-bs-parent="#accordionExample"
           >
-            <div class="accordion-body">
+            <div className="accordion-body">
               <form onSubmit={handleSubjectSubmit}>
-                <input type="text" name="classname" />
-                
-                <Button type="submit">Add Class</Button>
+                <input
+                  type="text"
+                  name="subName"
+                  onChange={(e) => handleSubChange(e)}
+                />
+                <select
+                  name=""
+                  onChange={(e) => handleSubChange(e)}
+                >
+                  <option value="1">1st</option>
+                  <option value="2">2nd</option>
+                </select>
+                <Button type="submit">Add Subject</Button>
               </form>
             </div>
           </div>
         </div>
-        <div class="accordion-item">
-          <h2 class="accordion-header" id="headingThree">
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingThree">
             <button
-              class="accordion-button collapsed"
+              className="accordion-button collapsed"
               type="button"
               data-bs-toggle="collapse"
               data-bs-target="#collapseThree"
@@ -87,11 +120,11 @@ const SideBar = () => {
           </h2>
           <div
             id="collapseThree"
-            class="accordion-collapse collapse"
+            className="accordion-collapse collapse"
             aria-labelledby="headingThree"
             data-bs-parent="#accordionExample"
           >
-            <div class="accordion-body">
+            <div className="accordion-body">
               <form onSubmit={handleNotesSubmit}>
                 <input type="text" name="classname" />
                 <Button type="submit">Add Class</Button>
